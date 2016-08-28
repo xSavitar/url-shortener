78c32548cfc739b4654b6b107c6a0eca383eb738
Perside Rosalie <persiderosalie@gmail.com>
2016-08-28 23:37:00 +0100

    trimed the link and removed special characters from the link

--- javascript/shorten.js
+++ javascript/shorten.js
@@ -1,26 +1,23 @@
-$(function(){
+$('.btn-shorten').on('click', function(){
 
-    $('.btn-shorten').on('click', function(){
-
-        var protocol = $('#urlfield').val().split(':');
+    var protocol = $('#urlfield').val().split(':');
             
-        if ( protocol[0] != "http" ) {
-            alert( "Url must contain a protocol, e.g. http " );
-            return false;
+    if ( protocol[0] != "http" ) {
+        alert( "Url must contain a protocol, e.g. http " );
+        return false;
+    }
+
+    $.ajax({
+        url: "php-scripts/shorten.php",
+        type: "POST",
+        dataType: "JSON",
+
+        data: {link: $('#urlfield').val()},
+        success: function(data){
+            var resultHTML = '<strong>Copy Short URL: </strong>' + data.shortUrl;
+            $('#link').html(resultHTML);
+            $('#link').hide().fadeIn('slow');
         }
-
-        $.ajax({
-            url: "php-scripts/shorten.php",
-            type: "POST",
-            dataType: "JSON",
-
-            data: {link: $('#urlfield').val()},
-            success: function(data){
-                var resultHTML = '<strong>Copy Short URL: </strong>' + data.shortUrl;
-                $('#link').html(resultHTML);
-                $('#link').hide().fadeIn('slow');
-            }
-        });
-
     });
+
 });
--- javascript/validate.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    var str = document.forms["Form"]["link"].value;
-var protocol = str.split(":");
-    if ( protocol[0] != "http" ) {
-        alert( "Url must contain a protocol " + protocol[0] );
-        return false;
-    }
-}
